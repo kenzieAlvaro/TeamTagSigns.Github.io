fe3629b35fe3ca4fc8bbefb72dc8d2219d848719
kenzieAlvaro <kngg0100@student.monash.edu>
2025-05-21 16:05:23 +1000

    Add files via upload

---
+++ assignment 3/script.js
@@ -0,0 +1,5 @@
+document.getElementById("sendButton").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent form from submitting (optional)
+    alert("Your message has been sent!");
+  });
+  
